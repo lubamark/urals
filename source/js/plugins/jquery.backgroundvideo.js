e023793a49a4565c8c3e65e99845eb2c4777017e
lubamark <lubamark@mail.ru>
2018-07-23 01:11:32 +0700

    refactor plugin background

--- source/js/plugins/jquery.backgroundvideo.js
+++ source/js/plugins/jquery.backgroundvideo.js
@@ -61,7 +61,7 @@
         html += ' poster="' + plugin.settings.poster + '" ';
       }
 
-      html += 'style="display:none;position:fixed;top:0;left:0;bottom:0;right:0;z-index:-100;width:100%;height:100%;">';
+      html += 'style="display:none;position:absolute;top:0;left:0;bottom:0;right:0;z-index:-100;width:100%;height:100%;min-height:100%">';
       for(var i=0; i < plugin.settings.types.length; i++) {
         html += '<source src="'+plugin.settings.path+plugin.settings.filename+'.'+plugin.settings.types[i]+'" type="video/'+plugin.settings.types[i]+'" />';
       }
--- source/js/scripts/background.js
+++ source/js/scripts/background.js
@@ -1,14 +1,14 @@
-// $(document).ready(function() {
-//   var videobackground = new $.backgroundVideo($('.contract__video'), {
-//     "align": "centerX",
-//     "width": 1280,
-//     "height": 720,
-//     "path": "../../img/video/",
-//     "filename": "boat_classic",
-//     "types": ["mp4"],
-//     "preload": true,
-//     "autoplay": true,
-//     "loop": true
-//   });
-//   console.log('im done')
-// });
+$(document).ready(function() {
+  var videobackground = new $.backgroundVideo($('.contract__video'), {
+    "align": "centerX",
+    "width": 1280,
+    "height": 720,
+    "path": "../../img/video/",
+    "filename": "boat_classic",
+    "types": ["mp4"],
+    "preload": true,
+    "autoplay": true,
+    "loop": true
+  });
+  console.log('im done')
+});
